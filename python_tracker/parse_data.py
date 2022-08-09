571ed2e56cb075b8426588c1dba1c836fa83c8ab
Robertdebrus <robert@peterwallace.org>
2022-08-09 15:58:37 -0400

    update formatting

--- python_tracker/parse_data.py
+++ python_tracker/parse_data.py
@@ -16,31 +16,31 @@ def get_words(f1, f2):
             eye_data = json.load(eye_reader)
             page_data = json.load(page_boxes_reader)
             #print(eye_data)
-            i_data = eye_data['eye_data']
-            p_data = page_data['data']
-            for key in i_data.keys():
-                data = i_data[key]
-                if data['left_pupil_validity'] and data['right_pupil_validity']:
-                    x = m.width * data['right_gaze_point_on_display_area'][0]#((data['right_gaze_point_on_display_area'][0] + data['left_gaze_point_on_display_area'][0]) / 2) 
-                    y = m.height * data['right_gaze_point_on_display_area'][1]#((data['right_gaze_point_on_display_area'][1] + data['left_gaze_point_on_display_area'][1]) / 2)
-                    for div_data in p_data:
-                        for datum in div_data:
-                            word, *box = datum
-                            box = box[0]
-                            #print(box['left'] ,x , box['right'], '|||', box['top'] , y , box['bottom'])
-                            if (box['left'] < x < box['right']) and (box['top'] < y < box['bottom']):
-  
-                                if word in words_dict:
-                                    #print(words_dict[word][1][0], x)
-                                    if words_dict[word][1][0] != box['x']:
-                                        count, coords, appearance = words_dict[word]
-                                        words_dict[word + '_' + str(appearance + 1)] = [1, [box['x'],box['y']], appearance + 1]
-                                    else:
-                                        words_dict[word][0] += 1
-                                else:
-                                    words_dict[word] = [1, [box['x'],box['y']], 1]
-        for key in sorted(words_dict):
-            print(key, words_dict[key])
+    i_data = eye_data['eye_data']
+    p_data = page_data['data']
+    for key in i_data.keys():
+        data = i_data[key]
+        if data['left_pupil_validity'] and data['right_pupil_validity']:
+            x = m.width * data['right_gaze_point_on_display_area'][0]#((data['right_gaze_point_on_display_area'][0] + data['left_gaze_point_on_display_area'][0]) / 2) 
+            y = m.height * data['right_gaze_point_on_display_area'][1]#((data['right_gaze_point_on_display_area'][1] + data['left_gaze_point_on_display_area'][1]) / 2)
+            for div_data in p_data:
+                for datum in div_data:
+                    word, *box = datum
+                    box = box[0]
+                    #print(box['left'] ,x , box['right'], '|||', box['top'] , y , box['bottom'])
+                    if (box['left'] < x < box['right']) and (box['top'] < y < box['bottom']):
+
+                        if word in words_dict:
+                            #print(words_dict[word][1][0], x)
+                            if words_dict[word][1][0] != box['x']:
+                                count, coords, appearance = words_dict[word]
+                                words_dict[word + '_' + str(appearance + 1)] = [1, [box['x'],box['y']], appearance + 1]
+                            else:
+                                words_dict[word][0] += 1
+                        else:
+                            words_dict[word] = [1, [box['x'],box['y']], 1]
+    for key in sorted(words_dict):
+        print(key, words_dict[key])
 #        print(sorted(words_dict))
     
 
